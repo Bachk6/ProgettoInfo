fc0c4acdfbcfccdf2b0371caf54fee57d4a99efc
LostAstra7 <152957763+LostAstra7@users.noreply.github.com>
2025-05-09 11:55:27 +0200

    Inizio scrittura prenotazione

--- gst_prenotazioni.php
+++ gst_prenotazioni.php
@@ -3,7 +3,9 @@
     include("SQLconnect.php");
     CheckUser(array('admin','client'));
     $proiez_id = $_GET["proiezioneId"];
-    $data_proiez = $conn->query("SELECT giorno FROM proiezioni WHERE proiezioniId=$proiez_id");
+    $data_proiez = $conn->query("SELECT giorno FROM proiezioni WHERE proiezioneId=$proiez_id");
+
+
     
     //gst_prenotazioni?proiezioneid=3
     //uscire se data proiezione è <= oggi
@@ -18,7 +20,33 @@
         <title>Prenotazioni</title>
     </head>
     <body>
-        <p><?php echo "Proiezione ID: ".$proiez_id ?></p>
+<?php 
+    if($data_proiez > gmdate('Y-m-d')){
+        echo "<h3>SALA</h3>";
+        $row = 7;
+        $col = 7;
+        $table = "<table id='table' name='table'>";
+        for($i=0; $i<$col; $i++){
+            $table.= "<tr>";
+            for($j=0; $j<$row; $j++){
+                $table.="<td><input type='radio' name='posto' value='$i-$j'></td>";
+            }
+            $table.="</tr>";
+        }
+        $table.="</table>";
+        echo $table;
+
+        $form = "<form method='post' action=''>";
+        $form.= "<button type='submit'>Prenota</button>";
+        $form.= "</form>";
+        echo $form;
+    }
+    else
+    {
+        "<h3>Data del film non compatibile.</h3>";
+    }
+?>
+
     </body>
 </html>
 
